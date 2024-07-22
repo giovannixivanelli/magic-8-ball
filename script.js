226e6a3e8c69d6fecdcdc5f83312fbd016031cc5
Ivan Shakin <ivanxshakin1993@gmail.com>
2024-07-23 00:19:43 +0300

    design changes

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
-let randomNumber = Math.floor(Math.random() * 8);
-
-let eightBall = "";
 function displayNumberAnswer() {
+  let divContainer = document.getElementById("ball-answer");
+  setTimeout(
+    divContainer.setAttribute(
+      "style",
+      "color: rgb(205, 235, 255); opacity: 1; transition-duration: 5s; "
+    ),
+    50
+  );
   var number = Math.floor(Math.random() * 8);
-  document.getElementById("ball-number").innerHTML = number;
   switch (number) {
     case 1:
-      document.getElementById("ball-answer").innerHTML = "<p>It is certain</p>";
+      document.getElementById("ball-answer").innerHTML =
+        "<p>It is<br>certain</p>";
       break;
     case 2:
       document.getElementById("ball-answer").innerHTML =
-        "<p>It is decidedly so</p>";
+        "<p>It is<br>decidedly so</p>";
       break;
     case 3:
       document.getElementById("ball-answer").innerHTML =
-        "<p>Reply hazy try again</p>";
+        "<p>Reply hazy<br>try again</p>";
       break;
     case 4:
       document.getElementById("ball-answer").innerHTML =
-        "<p>Cannot predict now</p>";
+        "<p>Cannot<br>predict now</p>";
       break;
     case 5:
       document.getElementById("ball-answer").innerHTML =
-        "<p>Do not count on it</p>";
+        "<p>Do not<br>count on it</p>";
       break;
     case 6:
       document.getElementById("ball-answer").innerHTML =
-        "<p>My sources say no</p>";
+        "<p>My sources<br>say no</p>";
       break;
     case 7:
       document.getElementById("ball-answer").innerHTML =
-        "<p>Outlook not so good</p>";
+        "<p>Outlook<br>not so good</p>";
       break;
     case 8:
       document.getElementById("ball-answer").innerHTML =
-        "<p>Signs point to yes</p>";
+        "<p>Signs point<br>to yes</p>";
       break;
     default:
-      document.getElementById("ball-answer").innerHTML = "<p>Let's try again";
+      document.getElementById("ball-answer").innerHTML =
+        "<p>Let's<br>try again";
       break;
   }
+  /* document.getElementById("ball-button").onclick = myFunction;
+
+  function myFunction() {
+    clearTimeout(divContainer);
+  }*/
 }
